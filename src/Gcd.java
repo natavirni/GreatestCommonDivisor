9af5f14b5f3a8a6d7823f799b188036bdfa14180
Natashia Virnilia <61217073+natavirni@users.noreply.github.com>
2020-03-09 15:14:05 +0700

    Add files via upload

--- src/Gcd.java
+++ src/Gcd.java
@@ -12,15 +12,25 @@ public class Gcd {
         int number2 = input.nextInt();
         int gcd = 1;
 
-        if ((number1 > number2 && number1 % number2 == 0) || (number2 > number1 && number2 % number1 == 0) || number1 == number2) {
-            System.out.println("Greatest common divisor: " + (number1 > number2 ? number2 : number1));
+        int smallerOne = (number1 > number2 ? number2 : number1);
+        
+        if (isDivisor(number1, number2) || isDivisor(number2, number1)){
+            System.out.println("Greatest common divisor: " + smallerOne);
         } else{
-            for (int i = 1; i <= (number1 > number2 ? number2 : number1) / 2; i++) {
-                if (number1 % i == 0 && number2 % i == 0) {
-                    gcd = i;
+            for (int divisor = 1; divisor <= smallerOne / 2; divisor++) {
+                if (canBeDivide(number1, number2, divisor)) {
+                    gcd = divisor;
                 }
             }
             System.out.println("Greatest common divisor: " + gcd);
         }
+    }
+
+	private static boolean canBeDivide(int number1, int number2, int divisor) {
+		return number1 % divisor == 0 && number2 % divisor == 0;
+	}
+
+    private static boolean isDivisor(int num1, int num2){
+    	return (num1 >= num2 && num1 % num2 == 0);
     }
 }
